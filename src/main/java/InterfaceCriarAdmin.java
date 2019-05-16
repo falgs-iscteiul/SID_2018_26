5cafc7f68e0aefe846cd8a1b3ccf22a380a7678f
Filipe Gonçalves <falgs@iscte-iul.pt>
2019-05-16 12:08:33 +0100

    Interface que permite criar utilizadores

--- src/main/java/InterfaceCriarAdmin.java
+++ src/main/java/InterfaceCriarAdmin.java
@@ -26,6 +26,8 @@ public class InterfaceCriarAdmin extends JFrame {
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
+	private JTextField textField_2;
+
 
 	/**
 	 * Launch the application.
@@ -48,48 +50,60 @@ public class InterfaceCriarAdmin extends JFrame {
 	 */
 	public InterfaceCriarAdmin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 492, 301);
+		setBounds(100, 100, 492, 355);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblCriarAdministrador = new JLabel("Criar Administrador");
+		JLabel lblCriarAdministrador = new JLabel("Criar Investigador");
 		lblCriarAdministrador.setFont(new Font("Tahoma", Font.BOLD, 20));
-		lblCriarAdministrador.setBounds(123, 13, 207, 16);
+		lblCriarAdministrador.setBounds(139, 13, 207, 22);
 		contentPane.add(lblCriarAdministrador);
 		
-		JLabel lblUsername = new JLabel("Username");
+		JLabel lblUsername = new JLabel("Email");
 		lblUsername.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblUsername.setBounds(12, 79, 100, 16);
 		contentPane.add(lblUsername);
 		
-		JLabel lblPassword = new JLabel("Password");
+		JLabel lblPassword = new JLabel("Nome");
 		lblPassword.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblPassword.setBounds(12, 132, 100, 16);
 		contentPane.add(lblPassword);
 		
+		JLabel lblCategoria = new JLabel("Categoria");
+		lblCategoria.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblCategoria.setBounds(12, 185, 100, 16);
+		contentPane.add(lblCategoria);
+		
 		textField = new JTextField();
-		textField.setBounds(161, 77, 146, 22);
+		textField.setBounds(161, 79, 146, 22);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(161, 130, 146, 22);
+		textField_1.setBounds(161, 132, 146, 22);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(161, 185, 146, 22);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
 		
 		JButton btnCriar = new JButton("Criar");
 		btnCriar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				String  user = textField.getText();
-				String  pass = textField_1.getText();
+				String  email = textField.getText();
+				String  nome = textField_1.getText();
+				String  categoria = textField_2.getText();
 				try {
-					Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","");
-					PreparedStatement inserted = con.prepareStatement("INSERT INTO tblogin (UserName, Password) VALUES ('"+user+"','"+pass+"')");
+					Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mysqlmain?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","");
+					PreparedStatement inserted = con.prepareStatement("INSERT INTO investigador (Email, NomeInvestigador, CategoriaProfessional) VALUES ('"+email+"','"+nome+"','"+categoria+"')");
 					inserted.executeUpdate();
 					textField.setText("");
 					textField_1.setText("");
+					textField_2.setText("");
 				} catch (SQLException e1) {
 					e1.printStackTrace();
 				}
@@ -98,16 +112,16 @@ public class InterfaceCriarAdmin extends JFrame {
 				}
 			}
 		});
-		btnCriar.setBounds(161, 185, 97, 25);
+		btnCriar.setBounds(161, 241, 97, 25);
 		contentPane.add(btnCriar);
 		
 		JButton btnCancelar = new JButton("Cancelar");
 		btnCancelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
+				dispose();
 			}
 		});
-		btnCancelar.setBounds(290, 185, 97, 25);
+		btnCancelar.setBounds(290, 241, 97, 25);
 		contentPane.add(btnCancelar);
 	}
 }
